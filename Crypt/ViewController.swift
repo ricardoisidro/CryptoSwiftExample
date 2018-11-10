2b67d6df60f51abc24018757ea8ea3e137f9cf9f
ricardo <ricardo.isidro.86@gmail.com>
2018-11-09 21:19:04 -0600

    Initial Commit

---
+++ Crypt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crypt
+//
+//  Created by Ricardo Isidro on 11/9/18.
+//  Copyright © 2018 Expression B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
